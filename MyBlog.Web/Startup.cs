488a3b969094670309425eed9314668d60cfc1a2
John Wittenauer <jdwittenauer@gmail.com>
2014-01-26 21:09:45 -0500

    Initial project commit to Git.

---
+++ MyBlog.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyBlog.Web.Startup))]
+namespace MyBlog.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
