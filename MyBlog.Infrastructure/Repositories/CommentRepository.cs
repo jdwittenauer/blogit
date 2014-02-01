c2b1eb2839ac1deeb0bf96f07a36cfedb3645a31
John Wittenauer <jdwittenauer@gmail.com>
2014-01-31 21:54:24 -0500

    Just starting to get the basics added.

--- MyBlog.Infrastructure/Repositories/CommentRepository.cs
+++ MyBlog.Infrastructure/Repositories/CommentRepository.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace MyBlog.Infrastructure
+namespace MyBlog.Infrastructure.Repositories
 {
-    public class Class1
+    public class CommentRepository
     {
+
     }
 }
