5f9f7c8e46339d8960c889d0d4897faf49f62dfe
John Wittenauer <jdwittenauer@gmail.com>
2014-02-13 22:29:01 -0500

    Adding some automapper configuration.

--- MyBlog.Web/App_Start/AutomapperConfig.cs
+++ MyBlog.Web/App_Start/AutomapperConfig.cs
@@ -24,6 +24,18 @@ namespace MyBlog.Web
 
         private static void CreateMaps()
         {
+            // Map from domain entities to data contracts
+            Mapper.CreateMap<MyBlog.Domain.Entities.Author, MyBlog.Services.DataContracts.Author>();
+            Mapper.CreateMap<MyBlog.Domain.Entities.Blog, MyBlog.Services.DataContracts.Blog>();
+            Mapper.CreateMap<MyBlog.Domain.Entities.Comment, MyBlog.Services.DataContracts.Comment>();
+            Mapper.CreateMap<MyBlog.Domain.Entities.Post, MyBlog.Services.DataContracts.Post>();
+
+            // Map from data contracts to domain entities
+            Mapper.CreateMap<MyBlog.Services.DataContracts.Author, MyBlog.Domain.Entities.Author>();
+            Mapper.CreateMap<MyBlog.Services.DataContracts.Blog, MyBlog.Domain.Entities.Blog>();
+            Mapper.CreateMap<MyBlog.Services.DataContracts.Comment, MyBlog.Domain.Entities.Comment>();
+            Mapper.CreateMap<MyBlog.Services.DataContracts.Post, MyBlog.Domain.Entities.Post>();
+
             Mapper.AssertConfigurationIsValid();
         }
     }
